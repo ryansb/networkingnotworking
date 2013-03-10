f6bc6e63bae9b398de10c0f5f4394441a5151aa9
Ryan Brown <ryansb@csh.rit.edu>
2013-03-10 19:30:11 -0400

    Multithread the discard server

--- networkingnotworking.go
+++ networkingnotworking.go
@@ -1,40 +1,48 @@
 package main
 
 import (
-    "net"
-    "fmt"
-    "os"
+	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Fprintf(os.Stderr, "Usage: [discard|echo]", os.Args[0])
-        os.Exit(1)
-    }
-    name := os.Args[1]
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: [discard|echo]", os.Args[0])
+		os.Exit(1)
+	}
+	name := os.Args[1]
 
-    if name == "discard" {
-        tcpAddr, err := net.ResolveTCPAddr("ip4", ":9")
-        checkError(err)
+	if name == "discard" {
+		tcpAddr, err := net.ResolveTCPAddr("ip4", ":9000")
+		checkError(err)
 
-        listener, err := net.ListenTCP("tcp", tcpAddr)
-        checkError(err)
+		listener, err := net.ListenTCP("tcp", tcpAddr)
+		checkError(err)
 
-        var b [512]byte
-        for {
-            conn, err := listener.Accept()
-            checkError(err)
-            conn.Read(b[0:])
-        }
-    }
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				continue
+			}
+			go handleDiscard(conn)
+		}
+	}
 
-    os.Exit(0)
+	os.Exit(0)
 }
 
+func handleDiscard(conn net.Conn) {
+	defer conn.Close()
+	var b [512]byte
+	for {
+		conn.Read(b[0:])
+	}
+}
 
 func checkError(err error) {
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
 }
